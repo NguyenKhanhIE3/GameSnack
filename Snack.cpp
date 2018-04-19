bd73365a93256e7a615d54022fc1bd7e8bc7e584
NguyenKhanhIE3 <37476894+NguyenKhanhIE3@users.noreply.github.com>
2018-04-19 22:14:37 +0700

    Update Snack.cpp

--- Snack.cpp
+++ Snack.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 #include<windows.h>
 #include<conio.h>
+#include<ctime>
 using namespace std ;
-# define width 85
-#define height 20
+# define width 45
+# define height 25
 
 void clrscr()
 {
@@ -44,6 +45,9 @@ void TextColor (int color)
 struct Toado{
     int x,y;
 };
+
+int tickSpeed ;
+int score;
 struct Snack {
     Toado dot[100];
     int n ;
@@ -57,8 +61,14 @@ struct food {
 };
 
 
-void khoitao(Snack &snack,food &hq){
 
+
+
+void khoitao(Snack &snack,food &hq){
+	//khoi tao gia tri ban dau ;
+
+	tickSpeed=70;
+	score=0;
  //khoi tao ran
  snack.n=3;
  snack.dot[0].x=8;
@@ -76,6 +86,7 @@ void khoitao(Snack &snack,food &hq){
 void hienthi(Snack snack ,food hq){
 
     clrscr();
+    TextColor(12);
     for (int i=0;i<width;i++){
         gotoXY(i,0);
         cout<<char (3);}
@@ -94,26 +105,33 @@ void hienthi(Snack snack ,food hq){
 
 
     //in ra dau ran
+    TextColor(14);
     gotoXY(snack.dot[0].x,snack.dot[0].y);
     cout<<char (2);
-    //in ra than ran
+    //in ra than ra than ran
+
     for (int i =1;i<snack.n;i++){
         gotoXY(snack.dot[i].x,snack.dot[i].y);
         cout<<"*";
     }
     //in ra hoa qua
+    TextColor(15);
     gotoXY(hq.x,hq.y);
     cout<<char (4);
+    //diemso
+    gotoXY(width+15,25 );
+    TextColor(2);
+    cout<<"Score :"<<score;
 }
 void chuyendong(Snack &snack){
     //cap nhat vi tri con ran
     for (int i =snack.n-1;i>0;i--)
         snack.dot[i]=snack.dot[i-1];
-    if(kbhit())//neu nguoi dung dang nhap vao 1 phim no do
+    if(_kbhit())//neu nguoi dung dang nhap vao 1 phim no do
     {
-        char c=_getch();
+        int  c=_getch();
         if (c=='w'||c=='W')snack.tt=up;
-        else if (c=='x'||c=='X')snack.tt=down;
+        else if (c=='s'||c=='S')snack.tt=down;
         else if (c=='A'||c=='a')snack.tt=lefts;
         else if (c=='d'||c=='D')snack.tt=rights ;
 
@@ -124,18 +142,71 @@ void chuyendong(Snack &snack){
     else if (snack.tt==rights)snack.dot[0].x++;
     else if (snack.tt==lefts )snack.dot[0].x--;
 }
-int main  ()
-{
-
-    Snack snack;
-    food hq;
-
-    khoitao(snack,hq);
-    while (1){
-
-        hienthi(snack,hq);
-        chuyendong(snack);
-        Sleep(100);
-
+int xuli(Snack &snack,food &hq ){
+	
+    //xu li ran cham bien
+ if (snack.dot[0].x>=width ||snack.dot[0].x<=0 ||snack.dot[0].y<=0||snack.dot[0].y>=height)
+      return -1;
+    //xu li ran can chinh no
+ for (int i=1;i<snack.n;i++){
+     if (snack.dot[i].x==snack.dot[0].x&& snack.dot[i].y==snack.dot[0].y)
+    return -1 ;
     }
+
+
+
+ //xu li ran an hoa qua
+ if (snack.dot[0].x==hq.x&&snack.dot[0].y==hq.y){
+        for (int i=snack.n;i>0;i--)
+    snack.dot[i]=snack.dot[i-1];
+    snack.n++;
+    score++;
+    if (snack.tt==up)snack.dot[0].y--;
+    else if (snack.tt==down)snack.dot[0].y++;
+    else if (snack.tt==rights)snack.dot[0].x++;
+    else if (snack.tt==lefts )snack.dot[0].x--;
+
+    srand(time(NULL));
+    hq.x=2+rand ()%(width-4);
+    hq.y=2+rand()%(height-4);
+    if (tickSpeed>=7)
+        tickSpeed-=8;
+
+ }
+ return 0 ;
+
 }
+
+
+int main  ()
+{ 
+     Snack snack;
+     food hq;
+     int ma =0;
+    khoitao(snack,hq);
+ 
+    while (1){
+    hienthi(snack,hq);
+    chuyendong(snack);
+    ma =xuli(snack,hq);
+	
+  if (ma==-1){
+   TextColor(14);
+    gotoXY(width+5,6);
+    cout<<"\t\t\t ====        === \n\n\n\n\n";
+    gotoXY(width+5,10);
+    cout<<"\t\t\t    GAME OVER \n\n";
+    gotoXY(width+5,13);
+    cout<<"\t\t\t  *Your score : "<<score<<endl<<endl;
+        gotoXY(width+5,16);
+    cout<<"\t\t\t  *See you again !\n\n\n\n";
+    gotoXY(width+5,20);
+    cout<<"\t\t\t *****************";
+       while (_getch()!=13);
+       break;
+       
+
+    
+
+  }
+  Sleep(100);}
