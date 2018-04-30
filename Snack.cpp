767f02cece081def465ff8c4da024b157e7c9d38
NguyenKhanhIE3 <37476894+NguyenKhanhIE3@users.noreply.github.com>
2018-04-30 15:56:16 +0700

    Update Snack.cpp

--- Snack.cpp
+++ Snack.cpp
@@ -3,7 +3,7 @@
 #include<conio.h>
 #include<ctime>
 using namespace std ;
-# define width 45
+# define width  80
 # define height 25
 
 void clrscr()
@@ -42,171 +42,256 @@ void TextColor (int color)
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE) , color);
 }
-struct Toado{
-    int x,y;
+
+struct Moto//mot o
+{
+	char kitu;
+	int mau;
 };
 
-int tickSpeed ;
-int score;
-struct Snack {
+Moto buffer [height][(width/2)+1];
+
+void VeMoto(int dong ,int cot ,int kitu ,int mau =7)
+{
+
+	buffer[dong][cot].kitu = kitu;
+	buffer[dong][cot].mau = mau ;
+
+}
+
+struct Toado
+{
+    int x,y;//x la hoanh do ,y la tung do
+};
+
+
+
+struct Snake
+{
     Toado dot[100];
-    int n ;
     Trangthai tt;
+    int n ;
+    int score;
+    int tickSpeed;
 
 };
-struct food {
+struct food
+{
 
+    int x,y;//x la hoanh do ,y la tung do ;
 
-    int x,y;
 };
 
-
-
-
-
-void khoitao(Snack &snack,food &hq){
+void Creat (Snake &snake ,food &hq)
+{
 	//khoi tao gia tri ban dau ;
 
-	tickSpeed=70;
-	score=0;
- //khoi tao ran
- snack.n=3;
- snack.dot[0].x=8;
- snack.dot[0].y=6;
- snack.dot[1].x=7;
- snack.dot[1].y=6;
- snack.dot[2].x=6;
- snack.dot[2].y=6;
- snack.tt=rights ;
- //khoi tao hoa qua
- hq.x=7;
- hq.y=4;
+	snake.tickSpeed = 70;
+	snake.score = 0;
+    //khoi tao ran
+    snake.n = 3;
+    snake.dot[0].x = 8;
+    snake.dot[0].y = 6;
+    snake.dot[1].x = 7;
+    snake.dot[1].y = 6;
+    snake.dot[2].x = 6;
+    snake.dot[2].y = 6;
+    snake.tt = rights ;
+    //khoi tao hoa qua
+    hq.x = 7;
+    hq.y = 4;
+    //khoi tao buffer
+    for (int i = 0;i < height ;i++)
+    {
+    	for (int j = 0;j < width/2;j++)
+    	{
+    		VeMoto(i,j,' ');
+		}
+	}
+
 
 }
-void hienthi(Snack snack ,food hq){
 
-    clrscr();
-    TextColor(12);
-    for (int i=0;i<width;i++){
-        gotoXY(i,0);
-        cout<<char (3);}
-        for (int i=0;i<width;i++){
-        gotoXY(i,height);
-        cout<<char (4);
-    }
-    for (int j=0;j<height;j++){
-        gotoXY(0,j);
-        cout<<char (5);}
-        for (int j =0;j<height;j++){
-        gotoXY(width,j);
-        cout<<char (6);
-
-    }
-
+void Display(Snake snake ,food hq)
+{
+    for(int i = 0;i < height ;i++)
+	{
+		VeMoto(i,0,4);
+		VeMoto(i,width/2,4);
+	}
+	for (int j = 0;j < width/2;j++)
+    {
+		VeMoto(0,j,4);
+		VeMoto(height-1,j,4);
+	}
 
     //in ra dau ran
-    TextColor(14);
-    gotoXY(snack.dot[0].x,snack.dot[0].y);
-    cout<<char (2);
-    //in ra than ra than ran
 
-    for (int i =1;i<snack.n;i++){
-        gotoXY(snack.dot[i].x,snack.dot[i].y);
-        cout<<"*";
+    VeMoto(snake.dot[0].y,snake.dot[0].x,178,14);
+    //in ra than ra than ran
+    for (int i = 1;i < snake.n;i++)
+    {
+    	VeMoto(snake.dot[i].y,snake.dot[i].x,178,14);
     }
     //in ra hoa qua
-    TextColor(15);
-    gotoXY(hq.x,hq.y);
-    cout<<char (4);
-    //diemso
-    gotoXY(width+15,25 );
-    TextColor(2);
-    cout<<"Score :"<<score;
-}
-void chuyendong(Snack &snack){
-    //cap nhat vi tri con ran
-    for (int i =snack.n-1;i>0;i--)
-        snack.dot[i]=snack.dot[i-1];
-    if(_kbhit())//neu nguoi dung dang nhap vao 1 phim no do
+    VeMoto(hq.y,hq.x,4,15);
+    //diem so
+    gotoXY((width/2)+15,23 );
+    TextColor(12);
+    cout<<"Score :"<<snake.score;
+    //in buffer ra man hinh
+    gotoXY(0,0);
+    for (int i = 0;i < height ;i++)
     {
-        int  c=_getch();
-        if (c=='w'||c=='W')snack.tt=up;
-        else if (c=='s'||c=='S')snack.tt=down;
-        else if (c=='A'||c=='a')snack.tt=lefts;
-        else if (c=='d'||c=='D')snack.tt=rights ;
-
-
-    }
-    if (snack.tt==up)snack.dot[0].y--;
-    else if (snack.tt==down)snack.dot[0].y++;
-    else if (snack.tt==rights)snack.dot[0].x++;
-    else if (snack.tt==lefts )snack.dot[0].x--;
+    	for (int j = 0;j <= width/2;j++)
+    	{
+    		TextColor(buffer[i][j].mau);
+    		cout<<buffer[i][j].kitu;
+    		buffer[i][j].kitu = ' ';
+		}
+		if (i < height -1)
+			cout<<"\n";
+	}
 }
-int xuli(Snack &snack,food &hq ){
-	
+void Move_1 (Snake &snake )
+{
+    //cap nhat vi tri con ran
+    for (int i = snake.n-1;i > 0;i--)
+        snake.dot[i] = snake.dot[i-1];
+
+    if ( GetAsyncKeyState(VK_UP))
+        snake.tt = up;
+    if ( GetAsyncKeyState(VK_DOWN))
+        snake.tt = down;
+    if ( GetAsyncKeyState(VK_LEFT))
+        snake.tt = lefts;
+    if ( GetAsyncKeyState(VK_RIGHT))
+        snake.tt = rights ;
+    if ( snake.tt == up )
+        snake.dot[0].y--;
+    if ( snake.tt == down )
+        snake.dot[0].y++;
+    if ( snake.tt == rights )
+        snake.dot[0].x++;
+    if ( snake.tt == lefts )
+        snake.dot[0].x--;
+}
+int Process (Snake & snake ,food &hq )
+{
+
     //xu li ran cham bien
- if (snack.dot[0].x>=width ||snack.dot[0].x<=0 ||snack.dot[0].y<=0||snack.dot[0].y>=height)
-      return -1;
+    if ( snake.dot[0].x >= (width/2 -1) ||snake.dot[0].x <= 0 ||snake.dot[0].y <= 0 ||snake.dot[0].y >=(height -1) )
+        return -1;
     //xu li ran can chinh no
- for (int i=1;i<snack.n;i++){
-     if (snack.dot[i].x==snack.dot[0].x&& snack.dot[i].y==snack.dot[0].y)
-    return -1 ;
+    for (int i = 1; i < snake.n;i++)
+
+        if( snake.dot[0].x == snake.dot[i].x && snake.dot[0].y == snake.dot[i].y)
+            return -1;
+    //xu li ran an hoa qua
+    if ( snake.dot[0].x == hq.x && snake.dot[0].y == hq.y)
+    {
+        for (int i = snake.n ; i > 0; i --)
+            snake.dot[i] = snake.dot[i-1];
+        snake.n++;
+        snake.score++;
+        hq.x = 2 + rand () %( (width/2)-6);
+        hq.y = 2 + rand () %( height-6);
+        if ( snake.tickSpeed >= 7 )
+            snake.tickSpeed -= 4;
+
     }
+    return 0 ;
 
+}
+void Main_Move (Snake &snake ,food &hq)
+{
+    int ma =0;
+    Creat (snake,hq);
+    do
+    {
+        Display(snake,hq);
+        Move_1 (snake);
+        ma = Process (snake ,hq);
 
+        if (ma == -1)
+        {
+            TextColor(11);
+            gotoXY((width/2)+5,6);
+            cout<<"\t\t ====        ==== \n\n\n\n\n";
+            gotoXY((width/2)+5,10);
+            cout<<"\t\t     GAME OVER \n\n";
+            gotoXY((width/2)+5,13);
+            cout<<"\t\t  *Your score : "<<snake.score<<endl<<endl;
+            gotoXY((width/2)+5,16);
+            cout<<"\t\t  *See you again !\n\n\n\n";
+            gotoXY((width/2)+5,20);
+            cout<<"\t\t *****************";
+            while (_getch()!=13);
+            break;
 
- //xu li ran an hoa qua
- if (snack.dot[0].x==hq.x&&snack.dot[0].y==hq.y){
-        for (int i=snack.n;i>0;i--)
-    snack.dot[i]=snack.dot[i-1];
-    snack.n++;
-    score++;
-    if (snack.tt==up)snack.dot[0].y--;
-    else if (snack.tt==down)snack.dot[0].y++;
-    else if (snack.tt==rights)snack.dot[0].x++;
-    else if (snack.tt==lefts )snack.dot[0].x--;
+        }
 
-    srand(time(NULL));
-    hq.x=2+rand ()%(width-4);
-    hq.y=2+rand()%(height-4);
-    if (tickSpeed>=7)
-        tickSpeed-=8;
+            Sleep(100);
+    }
+    while (1);
 
- }
- return 0 ;
-
+}
+bool Rules ()
+{
+    char button;
+    gotoXY(12,8);
+    cout<<"_________________________QUY TAC____________________________________";
+    gotoXY(12,10);
+    cout<<"Nguoi choi su dung cac phim mui ten tren ban phim de di chuyen ran \n";
+    gotoXY(12,12);
+    cout<<"Tro choi ket thuc khi ran tu can chinh no hoac dam vao tuong       \n";
+    gotoXY(12,14);
+    cout<<"Ban co muon quay tro lai menu ?\n"
+          "\t\t\t y/Y:co\n"
+          "\t\t\t n/N:khong";
+    if ( button == 'n' || button == 'N')
+        return false ;
+    if ( button == 'y' || button == 'Y')
+        return true ;
 }
 
 
 int main  ()
-{ 
-     Snack snack;
-     food hq;
-     int ma =0;
-    khoitao(snack,hq);
- 
-    while (1){
-    hienthi(snack,hq);
-    chuyendong(snack);
-    ma =xuli(snack,hq);
-	
-  if (ma==-1){
-   TextColor(14);
-    gotoXY(width+5,6);
-    cout<<"\t\t\t ====        === \n\n\n\n\n";
-    gotoXY(width+5,10);
-    cout<<"\t\t\t    GAME OVER \n\n";
-    gotoXY(width+5,13);
-    cout<<"\t\t\t  *Your score : "<<score<<endl<<endl;
-        gotoXY(width+5,16);
-    cout<<"\t\t\t  *See you again !\n\n\n\n";
-    gotoXY(width+5,20);
-    cout<<"\t\t\t *****************";
-       while (_getch()!=13);
-       break;
-       
+{
+	srand(time(NULL));
+    Snake snake ;
+    food hq;
+    char button;
+    do
+    {
+        TextColor(11);
+        gotoXY(10,8);
+        cout<<"_________________MAIN MENU________________\n\n";
+        gotoXY(12,10);
+        cout<<"\t\t"<<char(4)<<"  1 => Play Game \n\n";
+        gotoXY(12,12);
+        cout<<"\t\t"<<char(4)<<"  2 => Rules \n\n";
+        gotoXY(12,14);
+        cout<<"\t\t"<<char(4)<<"  3 => Quit \n\n";
+        gotoXY(12,16);
+        button=_getch();
+        if ( button == '1' )
+        {
+            clrscr();
+            Main_Move(snake,hq);
+        }
+        if ( button == '2' )
+        {
+            clrscr();
+            if (!Rules())
+                break;
+        }
+        if ( button == '3')
+        {
+             break;
+        }
 
-    
 
-  }
-  Sleep(100);}
+    }
+    while(1);
+}
